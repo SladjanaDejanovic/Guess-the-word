1eed53eafb3c9162cb7e764a793e58157cf54b84
slady <csl.bunny.2207@gmail.com>
2023-02-02 22:18:00 +0100

    number of guesses

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 - generate random word ✅
  - put a random word from array to be word we're guessing ✅
 
-- value of input
-- only put in one letter at the time
+- value of input ✅
+- only put in one letter at the time ✅
 
-- check if letter is there - if inpput is part of secretWord
+- check if letter is there - if input is part of secretWord ✅
 - put correct guessed letter on their spot
 - show word
 - show win if guessed the word correctly
@@ -25,23 +25,33 @@
 
 */
 
-// document.querySelector("play-again").addEventListener("click", function () {});
+// document.querySelector(".play-again").addEventListener("click", function () {});
 
 const words = ["unbeliebubble", "dorime", "lovely", "slady", "yuri"];
-let random = words[Math.floor(Math.random() * words.length)];
+// let play = true;
 const guessButton = document.querySelector(".guessBtn");
-const input = document.querySelector(".letter").value;
-let play = true;
+const input = document.querySelector(".letter").value.toUpperCase();
 const showWord = document.querySelector(".word-in-progress");
+const numberOfGuesses = document.querySelector(".number");
+let number = 8;
 
 //- generate random word
-let secretWord = random.toUpperCase();
+let random = words[Math.floor(Math.random() * words.length)].toUpperCase();
+let secretWord = random;
 console.log(secretWord);
+
 // - show _ instead of letters in a secret word
-showWord.innerHTML = random.replace(/./gi, "_");
+showWord.innerHTML = secretWord.replace(/./gi, "_");
 
 // - event on button guess
 guessButton.addEventListener("click", function () {
+  // - check if letter is there
   if (secretWord.includes(input)) {
+    // numberOfGuesses.innerHTML = "8";
+
+    console.log("test");
+  } else {
+    number--;
+    numberOfGuesses.innerHTML = number;
   }
 });
