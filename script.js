5cec1b0d941de96bc1902e4d453186eab907e153
slady <csl.bunny.2207@gmail.com>
2023-02-14 21:43:13 +0100

    unique wrong letter

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
 "use strict";
 
 /*
- - event on button guess
+ - event on button guess✅
 
-- strings of words in array? for secret word ✅
+- strings of words in array for secret word ✅
 - generate random word ✅
- - put a random word from array to be word we're guessing ✅
+ - put a random word from array to be word to guess ✅
 
 - value of input ✅
 - only put in one letter at the time ✅
 
-- check if letter is there - if input is part of secretWord 
-- put correct guessed letter on their spot
-- show word
+- check if letter is there - if input is part of secretWord ✅
+- put correct guessed letter on their spot✅
+- show word✅
 - show win if guessed the word correctly
 
- - lower number of guesses if wrong
+ - lower number of guesses if wrong✅
 - populate ul guessed letteres with guesses from input if wrong ✅
-- delete input after pushing wrong guess in array
- 
+- delete input after pushing wrong guess in array✅
+ - prevent duplicate wrong guesses shown ✅
+ - dont lower number of guesses if letter already tried
  - show end game if no more guesses remained
  - show try again button
 
  - event on button play again (playing = false  ???)
 
 */
-
+// let play = true;
 // document.querySelector(".play-again").addEventListener("click", function () {});
 
 const words = ["unbeliebubble", "dorime", "lovely", "slady", "yuri"];
-// let play = true;
+
 const guessButton = document.querySelector(".guessBtn");
 const input = document.querySelector(".letter");
-
 const showWord = document.querySelector(".word-in-progress");
+const showWrong = document.querySelector(".guessed-letters");
+
 let numberOfGuesses = document.querySelector(".number");
 let number = 8;
 numberOfGuesses.innerHTML = number;
 
-const incorrectLetters = [];
-const showWrong = document.querySelector(".guessed-letters");
-
-// const correctGuesses = [];
 let correctGuess = "";
+
+// wrong guesses
+let incorrectLetters = [];
 
 //- generate random word
 let secretWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
 console.log(secretWord);
-// const stringArr = [...secretWord];
-// console.log(stringArr);
+
 let wordArray = secretWord.split("");
 let guessedWord = wordArray.map(() => "_");
 correctGuess = guessedWord.join("");
+
 // - show _ instead of letters in a secret word
 showWord.innerHTML = secretWord.replace(/./gi, "_");
-
-// const checkLetter = function () {};
 
 console.log(correctGuess);
 
@@ -63,17 +62,8 @@ console.log(correctGuess);
 guessButton.addEventListener("click", function () {
   const guess = input.value.toUpperCase();
   console.log(guess);
+
   // - check if letter is there
-
-  //maps
-  // let correctGuess = secretWord
-  //   .split("")
-  //   .map((char) => (char === letter ? char : "_"))
-  //   .join("");
-  // console.log(correctGuess);
-  // showWord.innerHTML = correctGuess;
-  // input.value = "";
-
   let wordArray = secretWord.split("");
   guessedWord = wordArray.map((letter, index) => {
     if (letter === guess) {
@@ -88,7 +78,8 @@ guessButton.addEventListener("click", function () {
 
   if (!secretWord.includes(guess)) {
     incorrectLetters.push(guess);
-    showWrong.innerHTML = [...incorrectLetters];
+    let uniqueLetter = [...new Set(incorrectLetters)];
+    showWrong.innerHTML = [...uniqueLetter];
     number--;
     numberOfGuesses.innerHTML = number;
     input.value = "";
@@ -98,54 +89,8 @@ guessButton.addEventListener("click", function () {
   //   console.log("done!");
   // }
 
-  // if (secretWord.includes(letter)) {
-  //   correctGuesses.push(letter);
-  //   console.log(correctGuesses);
-  //   // let correctWord = correctGuesses.toString();
-  //   // showWord.innerHTML = secretWord.replace(/letter/g);
-  //   input.value = "";
-  //   if (correctGuesses.length === stringArr.length) {
-  //     console.log("done!");
-  //   }
-  // } else {
-  //   console.log("test");
-  //   incorrectLetters.push(letter);
-  //   console.log(incorrectLetters);
-  //   showWrong.innerHTML = [...incorrectLetters];
-  //   number--;
-  //   numberOfGuesses.innerHTML = number;
-  //   input.value = "";
   //   if (number <= 0) console.log("game over");
   // }
+
   showWord.innerHTML = correctGuess;
 });
-
-// replace all occurrances of correct letter in secret word
-
-///////////////
-/*
-let word = "javascript";
-let input = document.querySelector("input");
-let correctGuess = "";
-
-let wordArray = word.split("");
-let guessedWord = wordArray.map(() => "_");
-
-correctGuess = guessedWord.join("");
-
-console.log(correctGuess);
-
-input.addEventListener("input", function() {
-  let guess = input.value;
-  wordArray = word.split("");
-  guessedWord = wordArray.map((letter, index) => {
-    if (letter === guess) {
-      return guess;
-    } else {
-      return correctGuess[index];
-    }
-  });
-
-  correctGuess = guessedWord.join("");
-  console.log(correctGuess);
-}); */
